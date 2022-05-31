535dbc9e6ce46085655a0cb7d2e95338d649897e
Josky <ceinnova02604@innovaccion.mx>
2022-05-31 18:22:01 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Curso de Pyhton para principiantes 
+En este curso aprenderas a programar en phyton 
+![](1200px-Python-logo-notext.svg.png)
+**Requisitos**
+En este curso aprenderas a programar en phyton 
+- Python 3.6 o superior 
+- Computadora con Windows o MacOS
+- Editor de textos como [Visual estudio code](https://code.visualstudio.com/download)
---
+++ hola mundo 2.py
@@ -0,0 +1,20 @@
+#Este es mi primer programa en phyton 
+#Lo primero que imprimi fue un hola mundo
+print("Hello World")
+nombre = input("Como te llamas: ")
+print ("Hola ", nombre)
+calificacion = input("Introduce tu calificacion: ")
+calificacion = int(calificacion)
+if calificacion <700:
+    print("No pasaste por no estudiar master")    #Utilizamos el operador if para saber un rango de cosas
+    print("Intentalo el siguiente año master")
+elif calificacion > 1000:
+    print("No es muy creible mi estimado")
+elif calificacion == 700:
+    print(("Pasaste de panzaso master"))
+else : 
+    print("Felicidades pasa por tu certificado")
+#En python no existe el switch case lamentablemente 
+#me quede en el minuto 1.22
+git config --global user.email "ceinnova02604@innovaccion.mx"
+git config --global user.name "Josky"
